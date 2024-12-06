444d938cd693448eadd9b2e877d991d10b569434
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-06 15:05:00 +0200

    reorder sections

--- main.go
+++ main.go
@@ -1,59 +1,68 @@
 package main
 
-import "fmt"
-
 func main() {
-	var conferenceName string = "Go Conference"
-	const conferenceTickets int = 50
-	remainingTickets := conferenceTickets
-	var bookings [50]string
-	var bookingSlice []string
-	fmt.Println("Hello World from GO")
-	//fmt.Print("Welcome to our Conference \"", conferenceName, "\" booking application\n")
-	//fmt.Print("We had total of ", conferenceTickets, " tickets, and the available are ", remainingTickets, " tickets\n")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket int
-
-	fmt.Print("Your first name : ")
-	fmt.Scan(&firstName)
-
-	fmt.Print("Your last name : ")
-	fmt.Scan(&lastName)
-
-	fmt.Print("Your email address : ")
-	fmt.Scan(&email)
-
-	fmt.Print("Ticket No : ")
-	fmt.Scan(&userTicket)
-
-	remainingTickets = remainingTickets - userTicket
-	bookings[0] = firstName + " " + lastName
-	bookingSlice = append(bookingSlice, firstName+" "+lastName)
-
-	fmt.Println("Array in GO")
-	fmt.Printf("Array all data %v\n", bookings)
-	fmt.Printf("Array first item %v\n", bookings[0])
-	fmt.Printf("Array type %T\n", bookings)
-	fmt.Printf("Array size %v\n", len(bookings))
-
-	fmt.Println("Slice in GO")
-	fmt.Printf("Slice all data %v\n", bookingSlice)
-	fmt.Printf("Slice first item %v\n", bookingSlice[0])
-	fmt.Printf("Slice type %T\n", bookingSlice)
-	fmt.Printf("Slice size %v\n", len(bookingSlice))
-
-	//fmt.Printf("Type of conferenceName is %T and ,conferenceTickets is %T ,and remainingTickets is %T \n", conferenceName, conferenceTickets, remainingTickets)
-	fmt.Printf("Welcome to our Conference \"%v\" for booking application\n", conferenceName)
-	fmt.Printf("We had total of %v tickets, and the available are %v tickets\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Thanks %v for booking %v tickets, you will receive a confirmation email on %v . \n", bookings[0], userTicket, email)
+	// 1.0 vars
 
 	// GO   	JAVA
 	// int8     byte
 	// int16    short
 	// int32    int
 	// int64    long
+
+	/*
+		var conferenceName string = "Go Conference"
+		const conferenceTickets int = 50
+		remainingTickets := conferenceTickets
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket int
+	*/
+
+	// 2.0 reading input
+
+	/*
+		fmt.Print("Your first name : ")
+		fmt.Scan(&firstName)
+
+		fmt.Print("Your last name : ")
+		fmt.Scan(&lastName)
+
+		fmt.Print("Your email address : ")
+		fmt.Scan(&email)
+
+		fmt.Print("Ticket No : ")
+		fmt.Scan(&userTicket)
+	*/
+
+	// 3.0 printing
+
+	/*
+		fmt.Println("Hello World from GO")
+		fmt.Print("Welcome to our Conference \"", conferenceName, "\" booking application\n")
+		fmt.Print("We had total of ", conferenceTickets, " tickets, and the available are ", remainingTickets, " tickets\n")
+		fmt.Printf("Welcome to our Conference \"%v\" for booking application\n", conferenceName)
+		fmt.Printf("We had total of %v tickets, and the available are %v tickets\n", conferenceTickets, remainingTickets)
+	*/
+
+	// 4.0 array and slice
+
+	/*
+		var bookings [50]string
+		var bookingSlice []string
+
+		fmt.Println("Array in GO")
+		fmt.Printf("Array all data %v\n", bookings)
+		fmt.Printf("Array first item %v\n", bookings[0])
+		fmt.Printf("Array type %T\n", bookings)
+		fmt.Printf("Array size %v\n", len(bookings))
+
+		fmt.Println("Slice in GO")
+		fmt.Printf("Slice all data %v\n", bookingSlice)
+		fmt.Printf("Slice first item %v\n", bookingSlice[0])
+		fmt.Printf("Slice type %T\n", bookingSlice)
+		fmt.Printf("Slice size %v\n", len(bookingSlice))
+	*/
 
 }
